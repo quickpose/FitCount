27a0292a9ea45c734c36d7f1c8c885790c928892
dvolkhonskiy <6569659+dvolkhonskiy@users.noreply.github.com>
2023-05-22 11:39:15 +0100

    Initial Commit

---
+++ FitCount/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FitCount
+//
+//  Created by Денис Волхонский on 22.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitCount/FitCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCountApp.swift
+//  FitCount
+//
+//  Created by Денис Волхонский on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FitCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
